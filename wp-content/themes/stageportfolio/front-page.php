e75ddd312f77e9bc60f33448f6c6eb25cab8d74f
Mustafa <mustimustafa5@gmail.com>
2021-09-23 16:53:46 +0200

    homepage nearly done.

--- wp-content/themes/stageportfolio/front-page.php
+++ wp-content/themes/stageportfolio/front-page.php
@@ -1,16 +1,30 @@
-<?php
-get_header();
-?>
 
-<div class="content">
+<div class="header">
 <?php
-the_content()
+
+$image = get_field('homepage_image');
+$picture = $image['sizes']['large'];
+
+get_header();
 ?>
 </div>
 
+<div class="flex-container" >
+    <div class="title_homepage">
+        <h1 class="line-animation">
+            <?php
+            the_field('page_title');
+            ?>
+        </h1>
+    </div>
+<div class="img-flex" >
+    <img src="<?php echo $picture; ?>" class="img_fluid">
+</div>
+</div>
+
 <div class="footer">
-<?php
-get_footer ();
-?>
+    <?php
+    get_footer();
+    ?>
 </div>
 
--- wp-content/themes/stageportfolio/header.php
+++ wp-content/themes/stageportfolio/header.php
@@ -3,7 +3,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="profile" href="https://mustafamaalin.nl">
     <?php wp_head(); ?>
 
 </head>
