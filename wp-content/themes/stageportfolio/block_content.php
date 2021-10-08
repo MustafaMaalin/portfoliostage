784ec687a14e4032a129fbcce99becea18b56ad9
Mustafa <mustimustafa5@gmail.com>
2021-10-08 16:52:49 +0200

    update

--- wp-content/themes/stageportfolio/block_content.php
+++ wp-content/themes/stageportfolio/block_content.php
@@ -1,18 +1,32 @@
 <?php /* Template Name: Content Stage */ ?>
 <?php get_header(); ?>
 
+<?php
+$image_project = get_field( 'image_project' );
+$pictures       = $image_project ['sizes']['large'];
+?>
+
 <?php if ( have_rows( 'contentporfolio' ) ): ?>
 
 	<?php while ( have_rows( 'contentporfolio' ) ): the_row();
 		$contenttitle = get_sub_field( 'titlecontent' );
 		$contenttext  = get_sub_field( 'descriptionportfolio' );
 		$contenttexts = get_sub_field( 'descriptionportfolio2' );
+
 		?>
+		<div class="positionh1">
+		<h1 class="titlecss"><?php echo $contenttitle ?></h1>
+		</div>
         <div class="content-stage">
-            <h1 class="titlecss"><?php echo $contenttitle ?></h1>
             <p><?php echo $contenttext ?></p>
-            <p><?php echo $contenttexts ?></p>
+            <img src="<?php echo $pictures; ?>" class="img-stage">
         </div>
+        <div class="box-3">
+          <div class="btn-three">
+            <span>HOVER ME</span>
+          </div>
+        </div>
+
 
 	<?php endwhile; ?>
 <?php endif; ?>
